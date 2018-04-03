ad8bc668c114373aca28f40067c8936e3e008fcf
Hajime Hoshi <hajimehoshi@gmail.com>
2018-04-03 23:01:51 +0900

    audio: Remove Size functions (#466)

--- audio/vorbis/vorbis.go
+++ audio/vorbis/vorbis.go
@@ -58,11 +58,6 @@ func (s *Stream) Length() int64 {
 	return s.size
 }
 
-// Size is deprecated as of version 1.6.0-alpha. Use Length instead.
-func (s *Stream) Size() int64 {
-	return s.Length()
-}
-
 type decoded struct {
 	data       []float32
 	totalBytes int
